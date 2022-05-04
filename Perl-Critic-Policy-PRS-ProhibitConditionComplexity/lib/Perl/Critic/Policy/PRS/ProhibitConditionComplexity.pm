1c01fb80964931331a6679bf92924548840e7669
mardem1 <104694616+mardem1@users.noreply.github.com>
2022-05-04 23:08:43 +0200

    Update ProhibitConditionComplexity.pm
    added POD to ProhibitConditionComplexity

--- Perl-Critic-Policy-PRS-ProhibitConditionComplexity/lib/Perl/Critic/Policy/PRS/ProhibitConditionComplexity.pm
+++ Perl-Critic-Policy-PRS-ProhibitConditionComplexity/lib/Perl/Critic/Policy/PRS/ProhibitConditionComplexity.pm
@@ -112,3 +112,65 @@ sub violates
 }
 
 1;
+
+__END__
+
+#-----------------------------------------------------------------------------
+
+=pod
+
+=head1 NAME
+
+Perl::Critic::Policy::PRS::ProhibitConditionComplexity
+
+=head1 AFFILIATION
+
+This policy is part of L<Perl::Critic::Policy::PRS|Perl::Critic::Policy::PRS>.
+
+=head1 DESCRIPTION
+
+This Policy approximates the McCabe score within a coditional block "eg if(...)".
+
+See L<http://en.wikipedia.org/wiki/Cyclomatic_complexity>
+
+It should help to find complex conditions, which should be extracted
+into subs, to be more testable.
+
+eg. from
+
+  if( $a && $b || $c > 20 ) { ... }
+
+to
+
+  if( _some_test ($a, $b, $c) ) { .. }
+
+  sub _some_test {
+    my ($a, $b, $c ) = @_;
+
+    return  $a && $b || $c > 20;
+  }
+
+=head1 CONFIGURATION
+
+The maximum acceptable McCabe can be set with the C<max_mccabe>
+configuration item. Any block with a McCabe score higher than
+this number will generate a policy violation. The default is 2.
+
+An example section for a F<.perlcriticrc>:
+
+  [PRS::ProhibitConditionComplexity]
+  max_mccabe = 1
+
+=head1 AUTHOR
+
+mardem1 <>
+
+=head1 COPYRIGHT
+
+Copyright (c) 2022 All rights reserved.
+
+This program is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself. The full text of this license
+can be found in the LICENSE file included with this module.
+
+=cut
