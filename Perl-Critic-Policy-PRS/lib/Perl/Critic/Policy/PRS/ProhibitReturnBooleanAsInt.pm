63518711e7bddaee8e4e990ab067e2b2513004a1
mardem1 <104694616+mardem1@users.noreply.github.com>
2022-05-08 10:36:45 +0200

    early abort it not return keyword in violates ProhibitReturnBooleanAsInt

--- Perl-Critic-Policy-PRS/lib/Perl/Critic/Policy/PRS/ProhibitReturnBooleanAsInt.pm
+++ Perl-Critic-Policy-PRS/lib/Perl/Critic/Policy/PRS/ProhibitReturnBooleanAsInt.pm
@@ -25,21 +25,21 @@ sub violates
 {
     my ( $self, $elem, undef ) = @_;
 
-    if ( 'return' eq $elem->content() && !is_hash_key( $elem ) ) {
-        my $sib = $elem->snext_sibling();
-
-        if ( $sib && $sib->isa( 'PPI::Token::Structure' ) && $SEMICOLON eq $sib->content() ) {
-            return;
-        }
-
-        if( $sib && $sib->isa('PPI::Token::Number') && '0' ne $sib->content() && '1' ne $sib->content() ) {
-            return;
-        }
-
-        return $self->violation( 'return desc', 'return expl', $elem );
+    if ( 'return' ne $elem->content() || is_hash_key( $elem ) ) {
+        return;
     }
 
-    return;
+    my $sib = $elem->snext_sibling();
+
+    if ( $sib && $sib->isa( 'PPI::Token::Structure' ) && $SEMICOLON eq $sib->content() ) {
+        return;
+    }
+
+    if ( $sib && $sib->isa( 'PPI::Token::Number' ) && '0' ne $sib->content() && '1' ne $sib->content() ) {
+        return;
+    }
+
+    return $self->violation( 'return desc', 'return expl', $elem );
 }
 
 1;
