f5ac77e1d83d07db0586d90f86838a492c0ee776
mardem1 <104694616+mardem1@users.noreply.github.com>
2022-05-08 12:29:29 +0200

    ProhibitReturnBooleanAsInt uses only the simplified regex version

--- Perl-Critic-Policy-PRS/lib/Perl/Critic/Policy/PRS/ProhibitReturnBooleanAsInt.pm
+++ Perl-Critic-Policy-PRS/lib/Perl/Critic/Policy/PRS/ProhibitReturnBooleanAsInt.pm
@@ -10,7 +10,7 @@ use warnings;
 our $VERSION = '0.01';
 
 use Readonly;
-use Perl::Critic::Utils qw( is_hash_key $SEVERITY_MEDIUM $SCOLON $TRUE $FALSE );
+use Perl::Critic::Utils qw( is_hash_key $SEVERITY_MEDIUM );
 
 use base 'Perl::Critic::Policy';
 
@@ -39,51 +39,6 @@ sub supported_parameters
     return;
 }
 
-sub _simplified_violates_check
-{
-    my ( $elem ) = @_;
-
-    my $return_line_content = $elem->content();
-    if ( !$return_line_content ) {
-        return $FALSE;
-    }
-
-    # fast regex violation check - eg. "return 1"; - "return (1); # comment" - "return 1 if ..."
-    if ( $return_line_content =~ /^\s*return\s*[(]?\s*[01]\s*[)]?\s*((if|unless)\s*[(]?\s*.+\s*[)]?\s*)?\s*;\s*$/io ) {
-        return $TRUE;
-    }
-
-    return $FALSE;
-}
-
-sub _detailed_violates_check
-{
-    my ( $elem ) = @_;
-
-    if ( $elem->isa( 'PPI::Structure::List' ) ) {
-        if ( $elem->content() !~ m/^[(][01][)]$/aaixmso ) {
-            return $FALSE;
-        }
-    }
-    elsif ( $elem->isa( 'PPI::Token::Structure' ) && $SCOLON eq $elem->content() ) {
-        return $FALSE;
-    }
-    elsif ( $elem->isa( 'PPI::Token::Number' ) && '0' ne $elem->content() && '1' ne $elem->content() ) {
-        return $FALSE;
-    }
-    else {
-        my $sib = $elem->snext_sibling();
-
-        if ( $sib && $sib->isa( 'PPI::Token::Operator' ) ) {
-            return $FALSE;
-        }
-    }
-
-    # also all additions return 0 if ... violates - no extra checks needed.
-
-    return $TRUE;
-}
-
 sub violates
 {
     my ( $self, $elem, undef ) = @_;
@@ -97,20 +52,17 @@ sub violates
         return;
     }
 
-    if ( _simplified_violates_check( $elem ) ) {
-        return $self->violation( $DESC, $EXPL, $elem );
-    }
-
-    my $child = $return_keyword->snext_sibling();
-    if ( !$child ) {
+    my $return_line_content = $elem->content();
+    if ( !$return_line_content ) {
         return;
     }
 
-    if ( _detailed_violates_check( $child ) ) {
-        return $self->violation( $DESC, $EXPL, $elem );
+    # fast regex violation check - eg. "return 1"; - "return (1); # comment" - "return 1 if ..."
+    if ( $return_line_content !~ /^\s*return\s*[(]?\s*[01]\s*[)]?\s*((if|unless)\s*[(]?\s*.+\s*[)]?\s*)?\s*;\s*$/io ) {
+        return;
     }
 
-    return;
+    return $self->violation( $DESC, $EXPL, $elem );
 }
 
 1;
