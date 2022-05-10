0f94dbb974577fcfb52cfc4791aac01bc663d763
mardem1 <104694616+mardem1@users.noreply.github.com>
2022-05-10 13:00:01 +0200

    perl dist created with run-module-starter.pl

---
+++ Mardem-RefactoringPerlCriticPolicies/lib/Perl/Critic/Policy/Mardem/ProhibitManyConditionsInSub.pm
@@ -0,0 +1,104 @@
+package Perl::Critic::Policy::Mardem::ProhibitManyConditionsInSub;
+
+use 5.010;
+use strict;
+use warnings;
+
+=head1 NAME
+
+Perl::Critic::Policy::Mardem::ProhibitManyConditionsInSub - The great new Perl::Critic::Policy::Mardem::ProhibitManyConditionsInSub!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Perl::Critic::Policy::Mardem::ProhibitManyConditionsInSub;
+
+    my $foo = Perl::Critic::Policy::Mardem::ProhibitManyConditionsInSub->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+mardem, C<< <mardem at cpan.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-mardem-refactoringperlcriticpolicies at rt.cpan.org>, or through
+the web interface at L<https://rt.cpan.org/NoAuth/ReportBug.html?Queue=Mardem-RefactoringPerlCriticPolicies>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Perl::Critic::Policy::Mardem::ProhibitManyConditionsInSub
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<https://rt.cpan.org/NoAuth/Bugs.html?Dist=Mardem-RefactoringPerlCriticPolicies>
+
+=item * CPAN Ratings
+
+L<https://cpanratings.perl.org/d/Mardem-RefactoringPerlCriticPolicies>
+
+=item * Search CPAN
+
+L<https://metacpan.org/release/Mardem-RefactoringPerlCriticPolicies>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+This software is copyright (c) 2022 by mardem.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+
+=cut
+
+1; # End of Perl::Critic::Policy::Mardem::ProhibitManyConditionsInSub
