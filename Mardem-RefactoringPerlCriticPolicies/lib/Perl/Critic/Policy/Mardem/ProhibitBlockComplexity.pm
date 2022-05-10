d598ebd03a50db1a6de72e4e5ea4bd3bedb9cca5
mardem1 <104694616+mardem1@users.noreply.github.com>
2022-05-10 13:23:40 +0200

    moved ProhibitBlockComplexity from dev into prod dist folder

--- Mardem-RefactoringPerlCriticPolicies/lib/Perl/Critic/Policy/Mardem/ProhibitBlockComplexity.pm
+++ Mardem-RefactoringPerlCriticPolicies/lib/Perl/Critic/Policy/Mardem/ProhibitBlockComplexity.pm
@@ -1,99 +1,137 @@
 package Perl::Critic::Policy::Mardem::ProhibitBlockComplexity;
 
+use utf8;
+
 use 5.010;
+
 use strict;
 use warnings;
 
 our $VERSION = '0.01';
 
-sub function1 {
+use Readonly;
+
+use Perl::Critic::Utils qw{ :severities :data_conversion :classification };
+use Perl::Critic::Utils::McCabe qw{ calculate_mccabe_of_main };
+
+use Mardem::RefactoringPerlCriticPolicies::Util qw( search_for_block_keyword );
+
+use base 'Perl::Critic::Policy';
+
+Readonly::Scalar my $EXPL => q{Consider refactoring};
+
+sub default_severity
+{
+    return $SEVERITY_MEDIUM;
 }
 
-sub function2 {
+sub default_themes
+{
+    return qw(complexity maintenance);
+}
+
+sub applies_to
+{
+    return 'PPI::Structure::Block';
+}
+
+sub supported_parameters
+{
+    return (
+        {   'name'            => 'max_mccabe',
+            'description'     => 'The maximum complexity score allowed.',
+            'default_string'  => '10',
+            'behavior'        => 'integer',
+            'integer_minimum' => 1,
+        },
+    );
+}
+
+sub violates
+{
+    my ( $self, $elem, undef ) = @_;
+
+    my $score = calculate_mccabe_of_main( $elem );
+    if ( $score <= $self->{ '_max_mccabe' } ) {
+        return;
+    }
+
+    my $block_keyword = search_for_block_keyword( $elem );
+    if ( !$block_keyword ) {
+        $block_keyword = 'no-keyword-found';
+    }
+
+    if ( 'SUB' eq $block_keyword ) {
+        return;    # no sub -> see SUB Perl::Critic::Policy::Subroutines::ProhibitExcessComplexity !
+    }
+
+    my $desc = qq<"$block_keyword" code-block has a high complexity score ($score)>;
+    return $self->violation( $desc, $EXPL, $elem );
 }
 
 1;
 
 __END__
 
+#-----------------------------------------------------------------------------
+
+=pod
+
+=encoding utf8
+
 =head1 NAME
 
-Perl::Critic::Policy::Mardem::ProhibitBlockComplexity - The great new Perl::Critic::Policy::Mardem::ProhibitBlockComplexity!
+Perl::Critic::Policy::Mardem::ProhibitBlockComplexity
 
-=head1 VERSION
+=head1 DESCRIPTION
 
-Version 0.01
+This Policy approximates the McCabe score within a code block "eg if() {...}".
 
-=head1 SYNOPSIS
+See L<http://en.wikipedia.org/wiki/Cyclomatic_complexity>
 
-Quick summary of what the module does.
+It should help to find complex code block, which should be extracted
+into subs, to be more testable.
 
-Perhaps a little code snippet.
+eg. from
 
-    use Perl::Critic::Policy::Mardem::ProhibitBlockComplexity;
-
-    my $foo = Perl::Critic::Policy::Mardem::ProhibitBlockComplexity->new();
+  if( $a ) {
     ...
+    ...
+    ...
+  }
 
-=head1 EXPORT
+to
 
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
+  if( $a ) {
+    do_something();
+  }
 
-=head1 SUBROUTINES/METHODS
+=head1 CONFIGURATION
 
-=head2 function1
+The maximum acceptable McCabe can be set with the C<max_mccabe>
+configuration item. Any block with a McCabe score higher than
+this number will generate a policy violation. The default is 10.
 
-=head2 function2
+An example section for a F<.perlcriticrc>:
+
+  [PRS::ProhibitBlockComplexity]
+  max_mccabe = 1
+
+=head1 AFFILIATION
+
+This policy is part of L<Mardem::RefactoringPerlCriticPolicies|Mardem::RefactoringPerlCriticPolicies>.
 
 =head1 AUTHOR
 
 mardem, C<< <mardem at cpan.com> >>
 
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-mardem-refactoringperlcriticpolicies at rt.cpan.org>, or through
-the web interface at L<https://rt.cpan.org/NoAuth/ReportBug.html?Queue=Mardem-RefactoringPerlCriticPolicies>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Perl::Critic::Policy::Mardem::ProhibitBlockComplexity
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker (report bugs here)
-
-L<https://rt.cpan.org/NoAuth/Bugs.html?Dist=Mardem-RefactoringPerlCriticPolicies>
-
-=item * CPAN Ratings
-
-L<https://cpanratings.perl.org/d/Mardem-RefactoringPerlCriticPolicies>
-
-=item * Search CPAN
-
-L<https://metacpan.org/release/Mardem-RefactoringPerlCriticPolicies>
-
-=back
-
-
-=head1 ACKNOWLEDGEMENTS
-
-
 =head1 LICENSE AND COPYRIGHT
 
-This software is copyright (c) 2022 by mardem.
+Copyright (c) 2022, mardem
 
 This is free software; you can redistribute it and/or modify it under
-the same terms as the Perl 5 programming language system itself.
-
+the same terms as the Perl 5 programming language system itself. The
+full text of this license can be found in the LICENSE file included
+with this module.
 
 =cut
