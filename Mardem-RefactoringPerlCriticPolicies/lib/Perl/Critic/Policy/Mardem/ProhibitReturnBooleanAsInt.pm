24ff9e5453bc1dcfd808e9232daa0bb9d1b0a9ca
mardem1 <104694616+mardem1@users.noreply.github.com>
2022-05-10 13:27:42 +0200

    moved ProhibitReturnBooleanAsInt from dev into prod dist folder

--- Mardem-RefactoringPerlCriticPolicies/lib/Perl/Critic/Policy/Mardem/ProhibitReturnBooleanAsInt.pm
+++ Mardem-RefactoringPerlCriticPolicies/lib/Perl/Critic/Policy/Mardem/ProhibitReturnBooleanAsInt.pm
@@ -1,100 +1,116 @@
 package Perl::Critic::Policy::Mardem::ProhibitReturnBooleanAsInt;
 
+use utf8;
+
 use 5.010;
+
 use strict;
 use warnings;
 
 our $VERSION = '0.01';
 
-sub function1 {
+use Readonly;
+use Perl::Critic::Utils qw( is_hash_key $SEVERITY_MEDIUM );
+
+use base 'Perl::Critic::Policy';
+
+## no critic (RequireInterpolationOfMetachars)
+Readonly::Scalar my $EXPL => q{Consider using some $false, $true or other available module implementation};
+
+Readonly::Scalar my $DESC => q{"return" statement with explicit "0/1"};
+
+sub default_severity
+{
+    return $SEVERITY_MEDIUM;
 }
 
+sub default_themes
+{
+    return qw(complexity maintenance);
+}
 
-sub function2 {
+sub applies_to
+{
+    return 'PPI::Statement::Break';
+}
+
+sub supported_parameters
+{
+    return;
+}
+
+sub violates
+{
+    my ( $self, $elem, undef ) = @_;
+
+    my $return_keyword = $elem->schild();    # not next element - need first child
+    if ( !$return_keyword ) {
+        return;
+    }
+
+    if ( 'return' ne $return_keyword->content() || is_hash_key( $return_keyword ) ) {
+        return;
+    }
+
+    my $return_line_content = $elem->content();
+    if ( !$return_line_content ) {
+        return;
+    }
+
+    # fast regex violation check - eg. "return 1"; - "return (1); # comment" - "return 1 if ..."
+    my $return        = q{return};
+    my $value         = q{[(]?\s*[01]\s*[)]?};
+    my $opt_condition = q{(?:(?:if|unless)\s*[(]?\s*.+\s*[)]?\s*)?};
+    my $regex         = qr/^\s*$return\s*$value\s*$opt_condition\s*;/aaixmso;
+
+    if ( $return_line_content !~ $regex ) {
+        return;
+    }
+
+    return $self->violation( $DESC, $EXPL, $elem );
 }
 
 1;
 
 __END__
 
+#-----------------------------------------------------------------------------
+
+=pod
+
+=encoding utf8
+
 =head1 NAME
 
-Perl::Critic::Policy::Mardem::ProhibitReturnBooleanAsInt - The great new Perl::Critic::Policy::Mardem::ProhibitReturnBooleanAsInt!
+Perl::Critic::Policy::Mardem::ProhibitReturnBooleanAsInt
 
-=head1 VERSION
+=head1 DESCRIPTION
 
-Version 0.01
+This Policy searches for C<return 1> and C<return 0> statements,
+which are mainly used for boolean meaning, but are less expressiv
+than direct use of some boolean eg. C<return $true>.
 
-=head1 SYNOPSIS
+There are many different modules available for true, false - use it!
 
-Quick summary of what the module does.
+=head1 CONFIGURATION
 
-Perhaps a little code snippet.
+No Configuration
 
-    use Perl::Critic::Policy::Mardem::ProhibitReturnBooleanAsInt;
+=head1 AFFILIATION
 
-    my $foo = Perl::Critic::Policy::Mardem::ProhibitReturnBooleanAsInt->new();
-    ...
-
-=head1 EXPORT
-
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
-
-=head1 SUBROUTINES/METHODS
-
-=head2 function1
-
-=head2 function2
+This policy is part of L<Mardem::RefactoringPerlCriticPolicies|Mardem::RefactoringPerlCriticPolicies>.
 
 =head1 AUTHOR
 
 mardem, C<< <mardem at cpan.com> >>
 
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-mardem-refactoringperlcriticpolicies at rt.cpan.org>, or through
-the web interface at L<https://rt.cpan.org/NoAuth/ReportBug.html?Queue=Mardem-RefactoringPerlCriticPolicies>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Perl::Critic::Policy::Mardem::ProhibitReturnBooleanAsInt
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker (report bugs here)
-
-L<https://rt.cpan.org/NoAuth/Bugs.html?Dist=Mardem-RefactoringPerlCriticPolicies>
-
-=item * CPAN Ratings
-
-L<https://cpanratings.perl.org/d/Mardem-RefactoringPerlCriticPolicies>
-
-=item * Search CPAN
-
-L<https://metacpan.org/release/Mardem-RefactoringPerlCriticPolicies>
-
-=back
-
-
-=head1 ACKNOWLEDGEMENTS
-
-
 =head1 LICENSE AND COPYRIGHT
 
-This software is copyright (c) 2022 by mardem.
+Copyright (c) 2022, mardem
 
 This is free software; you can redistribute it and/or modify it under
-the same terms as the Perl 5 programming language system itself.
-
+the same terms as the Perl 5 programming language system itself. The
+full text of this license can be found in the LICENSE file included
+with this module.
 
 =cut
