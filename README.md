bad1c9ad385847b966bda0618082e94643eccec8
mardem1 <104694616+mardem1@users.noreply.github.com>
2022-05-13 17:23:05 +0200

    added some more explaining documentation

--- README.md
+++ README.md
@@ -1,25 +1,27 @@
 
-# perl-refactoring-support
+# Mardem-PerlRefactoringSupport
 
 ## DESCRIPTION
 
-Perl-Critic Policies for simple and isolated Refactoring-Support
+Perl-Critic Policies for simple and isolated Refactoring-Support.
 
-This Perl-Crictic Policy-Modules should help where to start a safe refactoring in old leagacy Perl code.
+This Perl-Crictic Policy-Modules should help where to start a safe
+refactoring in old leagacy Perl code.
 
-The standard Perl-Critic Policies which can be used are:
+The McCabe complexity check within the standard Perl-Critic Module are a good
+overall starting point see:
 
 * [Perl::Critic::Policy::Modules::ProhibitExcessMainComplexity](https://metacpan.org/pod/Perl::Critic::Policy::Modules::ProhibitExcessMainComplexity)
 * [Perl::Critic::Policy::Subroutines::ProhibitExcessComplexity](https://metacpan.org/pod/Perl::Critic::Policy::Subroutines::ProhibitExcessComplexity)
 
-but these are for some bigger scans, so these new Policies should check (or begin) in smaller chunks:
+but these are for some bigger scans, so these new policies should check (or begin) in smaller chunks:
 
-* condition complexity if,while,for (...){}
-* subs has many conditionals if,while,for, ...
-* large regular Code-Block as statement count
-* regular Code-Block complexity {...}
-* large Subs as statement count
-* return boolean as int
+1. **ProhibitReturnBooleanAsInt** - return boolean as int "return 1;"
+1. **ProhibitConditionComplexity** - condition complexity "if/while/for/... (...){}"
+1. **ProhibitManyConditionsInSub** - subs has many conditionals "if, while, for, ..."
+1. **ProhibitLargeBlock** - large code block as statement count "{...}"
+1. **ProhibitBlockComplexity** - code block complexity "{...}"
+1. **ProhibitLargeSub** - large subs as statement count
 
 ## INSTALLATION
 
@@ -32,11 +34,31 @@ To install this module, run the following commands:
   ./Build install
 ```
 
+## SUPPORT AND DOCUMENTATION
+
+Each policy has its own detailed documentation.
+
+## BUG REPORTS
+
+Please report bugs on GitHub.
+
+The source code repository can be found at [https://github.com/mardem1/mardem-refactoring-perlcritic-policies](https://github.com/mardem1/mardem-refactoring-perlcritic-policies)
+
+## AUTHOR
+
+Markus Demml, mardem@cpan.com
+
 ## LICENSE AND COPYRIGHT
 
-This software is copyright (c) 2022 by mardem.
+Copyright (c) 2022, Markus Demml
 
-This is free software; you can redistribute it and/or modify it under
-the same terms as the Perl 5 programming language system itself. The
-full text of this license can be found in the LICENSE file included
+This library is free software; you can redistribute it and/or modify it
+under the same terms as the Perl 5 programming language system itself.
+The full text of this license can be found in the LICENSE file included
 with this module.
+
+## DISCLAIMER
+
+This package is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE.
--- lib/Mardem/RefactoringPerlCriticPolicies.pm
+++ lib/Mardem/RefactoringPerlCriticPolicies.pm
@@ -21,7 +21,7 @@ __END__
 
 =head1 NAME
 
-Mardem::RefactoringPerlCriticPolicies -
+Mardem::RefactoringPerlCriticPolicies - Perl-Critic policies for simple and isolated Refactoring-Support.
 
 =head1 VERSION
 
@@ -29,55 +29,76 @@ Version 0.01
 
 =head1 DESCRIPTION
 
-L<https://github.com/mardem1/mardem-refactoring-perlcritic-policies>
+This Perl-Crictic Policy-Modules should help where to start a safe
+refactoring in old leagacy Perl code.
 
-=head1 AFFILIATION
-
-This module has no functionality, but instead contains documentation for this distribution and acts as a means of
-pulling other modules into a bundle. All of the Policy modules contained herein will have an "AFFILIATION" section
-announcing their participation in this grouping.
-
-=head1 AUTHOR
-
-mardem, C<< <mardem1 at users.noreply.github.com> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-mardem-refactoringperlcriticpolicies at rt.cpan.org>, or through
-the web interface at L<https://rt.cpan.org/NoAuth/ReportBug.html?Queue=Mardem-RefactoringPerlCriticPolicies>.
-I will be notified, and then you'll automatically be notified of progress on your bug as I make changes.
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Mardem::RefactoringPerlCriticPolicies
-
-You can also look for information at:
+The McCabe complexity check within the standard Perl-Critic Module are a good
+overall starting point see:
 
 =over 4
 
-=item * RT: CPAN's request tracker (report bugs here)
+=item * L<Perl::Critic::Policy::Modules::ProhibitExcessMainComplexity>
 
-L<https://rt.cpan.org/NoAuth/Bugs.html?Dist=Mardem-RefactoringPerlCriticPolicies>
-
-=item * CPAN Ratings
-
-L<https://cpanratings.perl.org/d/Mardem-RefactoringPerlCriticPolicies>
-
-=item * Search CPAN
-
-L<https://metacpan.org/release/Mardem-RefactoringPerlCriticPolicies>
+=item * L<Perl::Critic::Policy::Subroutines::ProhibitExcessComplexity>
 
 =back
 
+but these are for some bigger scans, so these new policies should check (or begin) in smaller chunks:
+
+=head2 L<Perl::Critic::Policy::Mardem::ProhibitReturnBooleanAsInt>
+
+return boolean as int "return 1;"
+
+=head2 L<Perl::Critic::Policy::Mardem::ProhibitConditionComplexity>
+
+condition complexity "if/while/for/... (...){}"
+
+=head2 L<Perl::Critic::Policy::Mardem::ProhibitManyConditionsInSub>
+
+subs has many conditionals "if, while, for, ..."
+
+=head2 L<Perl::Critic::Policy::Mardem::ProhibitLargeBlock>
+
+large code block as statement count "{...}"
+
+=head2 L<Perl::Critic::Policy::Mardem::ProhibitBlockComplexity>
+
+code block complexity "{...}"
+
+=head2 L<Perl::Critic::Policy::Mardem::ProhibitLargeSub>
+
+large subs as statement count
+
+=head1 AFFILIATION
+
+This module has no functionality, but instead contains documentation for this
+distribution and acts as a means of pulling other modules into a bundle.
+All of the Policy modules contained herein will have an "AFFILIATION" section
+announcing their participation in this grouping.
+
+=head1 BUG REPORTS
+
+Please report bugs on GitHub.
+
+The source code repository can be found at L<https://github.com/mardem1//mardem-refactoring-perlcritic-policies>
+
+=head1 AUTHOR
+
+Markus Demml, mardem@cpan.com
+
 =head1 LICENSE AND COPYRIGHT
 
-Copyright (c) 2022, mardem
+Copyright (c) 2022, Markus Demml
 
-This is free software; you can redistribute it and/or modify it under
-the same terms as the Perl 5 programming language system itself. The
-full text of this license can be found in the LICENSE file included
+This library is free software; you can redistribute it and/or modify it
+under the same terms as the Perl 5 programming language system itself.
+The full text of this license can be found in the LICENSE file included
 with this module.
+
+=head1 DISCLAIMER
+
+This package is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE.
 
 =cut
